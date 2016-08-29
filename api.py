c98fb2fff4403820758fffac387420e1d4d525b4
Jonas <jmurib@gmail.com>
2016-08-29 15:58:30 +0200

    Replaces api.py with minimal django project :muscle:

--- api.py
+++ api.py
@@ -1,34 +1,23 @@
-from flask import Flask, jsonify
-from flask.ext.restful import reqparse, abort, Api, Resource
-from flask.ext.restful.inputs import boolean
+import sys
 
-app = Flask(__name__)
-api = Api(app)
-resource_fields={
-        'door': True
-        }
-parser=reqparse.RequestParser()
-parser.add_argument('door', type=boolean)
+from django.conf import settings
+from django.conf.urls import url
+from django.core.management import execute_from_command_line
+from django.http import HttpResponse
 
-class Door(Resource):
-    def get(self, key):
-        return {'isOpen': resource_fields}, 200 
+settings.configure(
+    DEBUG=True,
+    SECRET_KEY='A-random-secret-key!',
+    ROOT_URLCONF=sys.modules[__name__],
+)
 
-class DoorOpen(Resource):
-    def post(self, key):
-        if key in resource_fields:
-            resource_fields[key]=True
-            return resource_fields[key], 200
 
-class DoorClosed(Resource):
-    def post(self, key):
-        if key in resource_fields:
-            resource_fields[key]=False
-            return resource_fields[key], 200
-	
-api.add_resource(Door, '/api/<string:key>')
-api.add_resource(DoorOpen, '/api/<string:key>/open')
-api.add_resource(DoorClosed, '/api/<string:key>/closed')
+def index(request):
+    return HttpResponse('<h1>A minimal Django response!</h1>')
+
+urlpatterns = [
+    url(r'^$', index),
+]
 
 if __name__ == '__main__':
-    app.run()
+    execute_from_command_line(sys.argv)
